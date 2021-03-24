d5be8d42f016be4f0d4b55a71049d209788944ad
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-24 23:49:15 +0000

    Added more.

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('button').addEventListener('click', comparison)
+
+
+
+
+function comparison() {
+  userInput = document.getElementById('input'). value
+  userInput = parseInt(userInput)
+
+  if (userInput < 0) {
+    document.getElementById('answer').innerHTML = " Negative"
+  } else {
+    document.getElementById('answer').innerHTML = "Positive"
+  }
+}
