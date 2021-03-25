c19cd6f44934998bea88bd81247ade0aa5b4871c
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-25 00:08:12 +0000

    fixed errors.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 document.getElementById('button').addEventListener('click', comparison)
 
-
-
-
-function comparison() {
+function comparison () {
   userInput = document.getElementById('input'). value
   userInput = parseInt(userInput)
 
   if (userInput < 0) {
-    document.getElementById('answer').innerHTML = " Negative"
+    document.getElementById('answer').innerHTML = 'Negative'
   } else {
-    document.getElementById('answer').innerHTML = "Positive"
+    document.getElementById('answer').innerHTML = 'Positive'
   }
 }
